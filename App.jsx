36689ba557a43226b5888cd8f924cdd8ae719d25
Christian-D-K <christian.d.k.505@gmail.com>
2021-10-30 14:00:26 +0900

    fix keyboard

--- App.jsx
+++ App.jsx
@@ -2,14 +2,14 @@ import React from 'react';
 
 // import MemoListScreen from './src/screens/MemoListScreen';
 // import MemoDetailScreen from './src/screens/MemoDetailScreen';
-import MemoCreateScreen from './src/screens/MemoCreateCsreen';
-// import MemoEditScreen from './src/screens/MemoEditScreen';
+// import MemoCreateScreen from './src/screens/MemoCreateCsreen';
+import MemoEditScreen from './src/screens/MemoEditScreen';
 
 export default function App() {
   return (
     // <MemoListScreen />
     // <MemoDetailScreen />
-    // <MemoEditScreen />
-    <MemoCreateScreen />
+    <MemoEditScreen />
+    // <MemoCreateScreen />
   );
 }
